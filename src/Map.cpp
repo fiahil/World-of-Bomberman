58b97bcf3febc6f59b22c023a310ff0586066257
busina_b <benjamin.businaro@gmail.com>
2012-05-10 17:05:55 +0200

    module map (generation | recuperation)

--- src/Map.cpp
+++ src/Map.cpp
@@ -5,6 +5,10 @@
  * Purpose: Implementation of the class Map
  ***********************************************************************/
 
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <sstream>
 #include "Map.h"
 
 ////////////////////////////////////////////////////////////////////////
@@ -29,12 +33,35 @@ Pattern Map::break(Pattern origin, std::list<Bonus>& bonus)
 // - y
 // - dwallDensity
 // - iwallDensity
-// Return:     
 ////////////////////////////////////////////////////////////////////////
 
 Map::Map(size_t x, size_t y, size_t dwallDensity, size_t iwallDensity)
+  : _x(x), _y(y), _map("")
 {
-   // TODO : implement
+  /* TODO Move */
+  time_t now;
+
+  time(&now);
+  srandom(now);
+  /* TODO Move */
+  for (size_t i = 0; i < (x * y); ++i)
+    this->_map += "0";
+  for (size_t ty = 0; ty < y; ++ty) {
+    for (size_t tx = 0; tx < x; ++tx) {
+      if ((tx == 0) || (ty == 0) ||
+          (tx == (x - 1)) || (ty == (y - 1)))
+        this->_map[POS(tx, ty)] = '1';
+      else if ((!(ty % 2) && !(tx % 2)))
+        {
+          if (!dwallDensity || !(random() % dwallDensity))
+            this->_map[POS(tx, ty)] = '1';
+	  else
+            this->_map[POS(tx, ty)] = '2';
+        }
+      else if (!iwallDensity || !(random() % iwallDensity))
+	this->_map[POS(tx, ty)] = '2';
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -42,18 +69,47 @@ Map::Map(size_t x, size_t y, size_t dwallDensity, size_t iwallDensity)
 // Purpose:    Implementation of Map::Map()
 // Parameters:
 // - file
-// Return:     
 ////////////////////////////////////////////////////////////////////////
 
 Map::Map(std::string file)
+  : _x(0), _y(0), _map("")
 {
-   // TODO : implement
+  std::string swap;
+  std::ifstream infile;
+  std::stringstream ss;
+
+  infile.open (file, std::ifstream::in);
+  if (!infile)
+    throw std::exception(); /* TODO BETTER */
+  std::getline(infile, swap);
+  ss << swap;
+  ss >> this->_x;
+  ss >> this->_y;
+  while (!infile.eof()) {
+    std::getline(infile, swap);
+    this->_map += swap;
+  }
+  if (this->_map.size() != (this->_x * this->_y))
+    throw std::exception(); /* TODO BETTER */
+}
+
+////////////////////////////////////////////////////////////////////////
+// Name:       Map::Map(size_t x, size_t y, std::string map)
+// Purpose:    Implementation of Map::Map()
+// Parameters:
+// - x
+// - y
+// - map
+////////////////////////////////////////////////////////////////////////
+
+Map::Map(size_t x, size_t y, std::string map)
+  : _x(x), _y(y), _map(map)
+{
 }
 
 ////////////////////////////////////////////////////////////////////////
 // Name:       Map::~Map()
 // Purpose:    Implementation of Map::~Map()
-// Return:     
 ////////////////////////////////////////////////////////////////////////
 
 Map::~Map()
@@ -66,7 +122,6 @@ Map::~Map()
 // Purpose:    Implementation of Map::Map()
 // Parameters:
 // - oldMap
-// Return:     
 ////////////////////////////////////////////////////////////////////////
 
 Map::Map(const Map& oldMap)
