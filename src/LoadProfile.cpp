64c85190ae635e118859399ecfffa46cf2a40d54
Sullivan <Sulli@sulli.(none)>
2012-05-27 15:17:40 +0200

    Added NewProfile + LoadProfile

---
+++ src/LoadProfile.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "LoadProfile.hpp"
+
+LoadProfile::LoadProfile(GameManager& game)
+  : AMenu("menu/Background3.png", "menu/Background3.png", 0.0f, -1.0f, 900.0f, game)
+{
+  this->_profiles = this->_profileLoader.getProfiles();
+
+  /*
+    One tag per profile available.
+    gotta see how to display with scrolling
+  */
+
+  this->_tags.push_back(new Tag("menu/BackNormal.png", "menu/BackHighlit.png", false, false, TokenMenu::MAINMENU, 790.0f, 0.0f, 1200.0f));
+}
+
+LoadProfile::~LoadProfile(void)
+{
+}
+
+double	LoadProfile::getCenterX() const
+{
+  return (800.0f);
+}
+
+double	LoadProfile::getCenterY() const
+{
+  return (1350.0f);
+}
---
+++ src/NewProfile.hpp
@@ -0,0 +1,21 @@
+/*
+ * Texan
+ * 27.05.2012
+ */
+
+#ifndef __NEWPROFILE_HPP__
+#define __NEWPROFILE_HPP__
+
+#include "AMenu.hpp"
+
+class	NewProfile : public AMenu
+{
+public:
+  NewProfile(GameManager&);
+  virtual ~NewProfile(void);
+
+  virtual double	getCenterX() const;
+  virtual double	getCenterY() const;
+};
+
+#endif
