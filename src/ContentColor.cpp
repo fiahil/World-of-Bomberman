899f46ae48387646f9b4f545e4e029a2e0bb87c1
lemonn_v <lemonn_v@epitech.eu>
2012-06-02 00:56:28 +0200

    Loading

--- src/ContentColor.cpp
+++ src/ContentColor.cpp
@@ -5,22 +5,24 @@
 
 #include "ContentColor.hpp"
 
-static const unsigned char g_refColor[7][3] = {
+static const unsigned char g_refColor[9][3] = {
   {255, 0, 0},
   {0, 255, 0},
   {0, 0, 255},
-  {255, 0, 255},
   {0, 255, 255},
   {255, 255, 0},
-  {127, 0, 0}
+  {127, 0, 0},
+  {127, 0, 127},
+  {0, 0, 0},
+  {127, 127, 127},
 };
 
 ContentColor::Color const	ContentColor::getColorFromId(size_t id)
 {
   ContentColor::Color	ret;
 
-  ret.R = g_refColor[id % 4][0];
-  ret.G = g_refColor[id % 4][1];
-  ret.B = g_refColor[id % 4][2];
+  ret.R = g_refColor[id % 9][0];
+  ret.G = g_refColor[id % 9][1];
+  ret.B = g_refColor[id % 9][2];
   return ret;
 }
