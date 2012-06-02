bad8d227ccb65a57315517bb23d883d1b05539d4
lemonn_v <lemonn_v@epitech.eu>
2012-06-02 19:47:41 +0200

    Factoring Bomberman

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,17 @@
-//
-// main.cpp for Bomberman in /home/lemonn_v/Projects/rendu/Bomberman
-// 
-// Made by vincent lemonnier
-// Login   <lemonn_v@epitech.net>
-// 
-// Started on  Thu May 10 22:24:44 2012 vincent lemonnier
-// Last update Wed May 30 20:09:01 2012 nicolas magere
-//
+/*
+ * lemonn_v
+ * 07.05.12
+ */
 
 #include <cstdlib>
 #include <exception>
 #include "Sound.hpp"
-#include "Menu.hpp"
+#include "Bomberman.hpp"
 #include "Error.hpp"
 
 int		main()
 {
-  Menu		bomberman;
+  Bomberman	bomberman;
   time_t	now;
 
   time(&now);
