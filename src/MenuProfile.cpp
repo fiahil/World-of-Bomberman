00ca5e3015e62a000df270021480dd9998ef3e13
lemonn_v <lemonn_v@epitech.eu>
2012-06-02 22:06:06 +0200

    Factoring MenuProfile

--- src/MenuProfile.cpp
+++ src/MenuProfile.cpp
@@ -6,19 +6,25 @@
 #include "MenuProfile.hpp"
 
 MenuProfile::MenuProfile(GameManager& game)
-  : AMenu("menu/background/backgroundGameMenu.jpg", "menu/background/backgroundGameMenu.jpg", 1600.0f, -1.0f, 800.0f, game)
+  : AMenu("menu/background/backgroundGameMenu.jpg", "menu/background/backgroundGameMenu.jpg",
+	  1600.0f, -1.0f, 800.0f, game)
 {
-  this->_tags.push_back(new Tag("menu/tags/NewGameNormal.png", "menu/tags/NewGameHighlit.png", true, false, TokenMenu::GAMECHOOSE, 2029.0f, 0.0f, 1000.0f));
-  this->_tags.push_back(new Tag("menu/tags/LoadGameNormal.png", "menu/tags/LoadGameHighlit.png", false, false, TokenMenu::LOADSAVE, 2029.0f, 0.0f, 1065.0f));
-  this->_tags.push_back(new Tag("menu/tags/SettingsNormal.png", "menu/tags/SettingsHighlit.png", false, false, TokenMenu::SETTINGSCHOOSE, 2029.0f, 0.0f, 1130.0f));
-  this->_tags.push_back(new Tag("menu/tags/StatNormal.png", "menu/tags/StatHighlit.png", false, false, TokenMenu::STATS, 2029.0f, 0.0f, 1195.0f));
-  this->_tags.push_back(new Tag("menu/tags/BackNormal.png", "menu/tags/BackHighlit.png", false, false, TokenMenu::MAINMENU, 2029.0f, 0.0f, 1260.0f));
-  this->_tags.push_back(new Tag("menu/tags/QuitNormal.png", "menu/tags/QuitHighlit.png", false, false, TokenMenu::QUIT, 2029.0f, 0.0f, 1325.0f));
+  this->_tags.push_back(new Tag("menu/tags/NewGameNormal.png", "menu/tags/NewGameHighlit.png",
+				true, false, TokenMenu::GAMECHOOSE, 2029.0f, 0.0f, 1000.0f));
+  this->_tags.push_back(new Tag("menu/tags/LoadGameNormal.png", "menu/tags/LoadGameHighlit.png",
+				false, false, TokenMenu::LOADSAVE, 2029.0f, 0.0f, 1065.0f));
+  this->_tags.push_back(new Tag("menu/tags/SettingsNormal.png", "menu/tags/SettingsHighlit.png",
+				false, false, TokenMenu::SETTINGSCHOOSE, 2029.0f, 0.0f, 1130.0f));
+  this->_tags.push_back(new Tag("menu/tags/StatNormal.png", "menu/tags/StatHighlit.png",
+				false, false, TokenMenu::STATS, 2029.0f, 0.0f, 1195.0f));
+  this->_tags.push_back(new Tag("menu/tags/BackNormal.png", "menu/tags/BackHighlit.png",
+				false, false, TokenMenu::MAINMENU, 2029.0f, 0.0f, 1260.0f));
+  this->_tags.push_back(new Tag("menu/tags/QuitNormal.png", "menu/tags/QuitHighlit.png",
+				false, false, TokenMenu::QUIT, 2029.0f, 0.0f, 1325.0f));
 }
 
 MenuProfile::~MenuProfile()
 {
-
 }
 
 double		MenuProfile::getCenterX() const
@@ -33,7 +39,8 @@ double		MenuProfile::getCenterY() const
 
 void		MenuProfile::update(gdl::GameClock const& clock, gdl::Input& input)
 {
-  for (size_t i = 0; i < this->_keyEvent.size(); ++i)
-    if (input.isKeyDown(this->_keyEvent[i].first))
-      (this->*_keyEvent[i].second)(clock);
+  for (vKeyEvent::const_iterator it = this->_keyEvent.begin();
+       it != this->_keyEvent.end(); ++it)
+    if (input.isKeyDown(it->first))
+      (this->*it->second)(clock);
 }
