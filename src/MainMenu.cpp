fd30bd3d8a5f4789f4c896db309d78278f3454a1
magere_n <nicolas.magere@epitech.eu>
2012-06-03 14:08:54 +0200

    Refactoring MainMenu

--- src/MainMenu.cpp
+++ src/MainMenu.cpp
@@ -33,8 +33,8 @@ double	MainMenu::getCenterY() const
 void	MainMenu::update(gdl::GameClock const& clock, gdl::Input& input)
 {
   for (size_t i = 0; i < this->_keyEvent.size(); ++i)
-    if (input.isKeyDown(this->_keyEvent[i].first))
-      (this->*_keyEvent[i].second)(clock);
+    if (input.isKeyDown(this->_keyEvent.at(i).first))
+      (this->*_keyEvent.at(i).second)(clock);
   if (this->_gameManager._mainProfile)
     this->_gameManager._mainProfile = 0;
 }
